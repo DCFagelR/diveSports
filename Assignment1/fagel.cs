29bcff83e68798b5bd345fb7aedac40567f4b5b1
Reinhart <fagelrdc@gmail.com>
2020-02-03 19:58:25 -0800

    added my hello world

---
+++ Assignment1/fagel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World\n");
+
+            Console.WriteLine("Press 'Enter' to exit...");
+            Console.ReadLine();
+        }
+    }
+}
