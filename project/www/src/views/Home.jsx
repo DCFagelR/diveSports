1fec575e84a3217b7c8ab44de8db986abfa4deee
Reeda10 <areed@chirotouch.com>
2020-02-16 20:17:32 -0800

    add initial shell for login

---
+++ project/www/src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+import Hero from '../components/Hero.jsx';
+import Content from '../components/Content.jsx';
+
+const Home = () => (
+  <Fragment>
+    <Hero />
+    <hr />
+    <Content />
+  </Fragment>
+);
+
+export default Home;
